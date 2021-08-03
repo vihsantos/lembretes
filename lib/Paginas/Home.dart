a83f98ea2f0cef3613fdde9811a7298775d5c68d
Vitoria <vick_santos_vick@hotmail.com>
2021-08-03 02:01:18 -0300

    Estruturando a home

--- lib/Paginas/Home.dart
+++ lib/Paginas/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lembretes/constantes/PaletaDeCores.dart';
 
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
@@ -10,6 +11,30 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "Lembretes",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontFamily: "Gowun Batang",
+                fontSize: 32,
+                color: PaletaDeCores.branco),
+          )),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+              PaletaDeCores.roxoum,
+              PaletaDeCores.amareloum,
+            ])),
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
