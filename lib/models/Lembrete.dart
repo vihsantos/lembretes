e73077f75ac52d8fec7c97b5099dc5425fa6c900
Vitoria <vick_santos_vick@hotmail.com>
2021-08-07 02:27:11 -0300

    Edição na model

--- lib/models/Lembrete.dart
+++ lib/models/Lembrete.dart
@@ -1,7 +1,30 @@
 class Lembrete {
   final String titulo;
   final String descricao;
-  final String data;
+  final DateTime data;
 
-  Lembrete(this.titulo, this.descricao, this.data);
+  Lembrete({this.titulo, this.descricao, this.data});
 }
+
+List<Lembrete> lembretes = [
+  Lembrete(
+      titulo: "Casa",
+      descricao: "comprar isso e aquilo, mas aquilo",
+      data: DateTime.now()),
+  Lembrete(
+      titulo: "Roupas",
+      descricao: "comprar isso e aquilo, mas aquilo",
+      data: DateTime.now()),
+  Lembrete(
+      titulo: "Vida",
+      descricao: "comprar isso e aquilo, mas aquilo",
+      data: DateTime.now()),
+  Lembrete(
+      titulo: "Blábláblá",
+      descricao: "comprar isso e aquilo, mas aquilo",
+      data: DateTime.now()),
+  Lembrete(
+      titulo: "Sonho",
+      descricao: "comprar isso e aquilo, mas aquilo",
+      data: DateTime.now()),
+];
