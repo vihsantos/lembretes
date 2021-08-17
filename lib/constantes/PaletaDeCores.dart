c5d75e5f5b532ace5f0b01d89553a791d80e38fa
Vitoria <vick_santos_vick@hotmail.com>
2021-08-17 17:23:35 -0300

    Adicionando mais cores na paleta de cores

--- lib/constantes/PaletaDeCores.dart
+++ lib/constantes/PaletaDeCores.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class PaletaDeCores {
   static Color amareloum = Color(0xFFFFF5C3);
   static Color roxoum = Color(0xFF9452A5);
+  static Color roxodois = Color(0xFF623AA2);
   static Color branco = Color(0xFFFFFFEE);
   static Color preto = Color(0xFF00000F);
+  static Color background = Color(0xFFededed);
 }
