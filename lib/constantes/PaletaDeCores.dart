dd86b20a5ce6058eb2c8c431ea9800d3c49bcced
Vitoria <vick_santos_vick@hotmail.com>
2021-09-04 00:03:34 -0300

    Adição de cores

--- lib/constantes/PaletaDeCores.dart
+++ lib/constantes/PaletaDeCores.dart
@@ -4,6 +4,8 @@ class PaletaDeCores {
   static Color amareloum = Color(0xFFFFF5C3);
   static Color roxoum = Color(0xFF9452A5);
   static Color roxodois = Color(0xFF623AA2);
+  static Color azulum = Color(0xFF6CC6CB);
+  static Color rosaum = Color(0xFFFE69BD);
   static Color branco = Color(0xFFFFFFEE);
   static Color preto = Color(0xFF00000F);
   static Color background = Color(0xFFededed);
