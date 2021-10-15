80c0b16d512fe4de497f0dc653d315375754c624
Vitoria <vick_santos_vick@hotmail.com>
2021-10-15 15:16:19 -0300

    Mudança da cor do card lembrete

--- lib/constantes/StylesDecoration.dart
+++ lib/constantes/StylesDecoration.dart
@@ -17,16 +17,22 @@ class StylesDecoration {
     ],
   );
 
-  static BoxDecoration decorationCardLembrete = BoxDecoration(boxShadow: [
-    BoxShadow(
-      offset: Offset(3, 3),
-      blurRadius: 3,
-      color: PaletaDeCores.roxoum.withOpacity(0.2),
-    ),
-    BoxShadow(
-      offset: Offset(-3, -3),
-      blurRadius: 3,
-      color: PaletaDeCores.amareloum.withOpacity(0.2),
-    )
-  ], borderRadius: BorderRadius.circular(30), color: PaletaDeCores.branco);
+  static BoxDecoration decorationCardLembrete = BoxDecoration(
+      boxShadow: [
+        BoxShadow(
+          offset: Offset(3, 3),
+          blurRadius: 3,
+          color: PaletaDeCores.roxoum.withOpacity(0.6),
+        ),
+        BoxShadow(
+          offset: Offset(-3, -3),
+          blurRadius: 3,
+          color: PaletaDeCores.amareloum.withOpacity(0.6),
+        )
+      ],
+      borderRadius: BorderRadius.circular(30),
+      gradient: LinearGradient(
+          begin: Alignment.bottomLeft,
+          end: Alignment.bottomRight,
+          colors: [PaletaDeCores.azulum, PaletaDeCores.azuldois]));
 }
