fb6efc6cda8e1fba6af36b31848066c3d293b425
Vitoria <vick_santos_vick@hotmail.com>
2021-10-22 18:46:11 -0300

    Adicionando TextStyles

--- lib/constantes/StylesText.dart
+++ lib/constantes/StylesText.dart
@@ -1,1 +1,31 @@
-class StylesText {}
+import 'package:flutter/material.dart';
+import 'package:lembretes/constantes/PaletaDeCores.dart';
+
+class StylesText {
+  static TextStyle tituloAppBarStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontFamily: "Gowun Batang",
+    fontSize: 32,
+    color: PaletaDeCores.preto,
+  );
+
+  static TextStyle tituloCardStyle = TextStyle(
+    fontFamily: "Gowun Batang",
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: PaletaDeCores.preto,
+  );
+
+  static TextStyle descricaoCardStyle = TextStyle(
+      color: PaletaDeCores.preto.withOpacity(0.6), fontStyle: FontStyle.italic);
+
+  static TextStyle dataCardStyle = TextStyle(
+      color: PaletaDeCores.preto.withOpacity(0.5), fontWeight: FontWeight.bold);
+
+  static TextStyle tituloHomeStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontFamily: "Gowun Batang",
+    fontSize: 24,
+    color: PaletaDeCores.preto,
+  );
+}
