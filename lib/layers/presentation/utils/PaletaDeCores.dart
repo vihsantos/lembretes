7054416953ec59535cfa8bc43dc2da5aced8a0ed
Vitoria <vick_santos_vick@hotmail.com>
2022-04-06 22:57:47 -0300

    Criando a paleta de cores

---
+++ lib/layers/presentation/utils/PaletaDeCores.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PaletaDeCores {
+  static Color background = Color(0xFFCBE6E8);
+  static Color roxo = Color(0xFF5B4266);
+  static Color branco = Color(0xFFFFF8F8);
+  static Color preto = Color(0xFF2D2A2F);
+}
