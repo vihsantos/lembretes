0597a71593b72d41a0242f800f280a57f609335c
Vitoria <vick_santos_vick@hotmail.com>
2022-04-09 22:57:17 -0300

    criando a pagina de criar lembrete

---
+++ lib/layers/presentation/pages/CriarLembrete.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CriarLembrete extends StatefulWidget {
+  const CriarLembrete({Key key}) : super(key: key);
+
+  @override
+  State<CriarLembrete> createState() => _CriarLembreteState();
+}
+
+class _CriarLembreteState extends State<CriarLembrete> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
