07b761b4d340bb3aec90a877dfbd1fa6e277fbe0
Vitoria <vick_santos_vick@hotmail.com>
2022-04-15 20:20:48 -0300

    adaptando a home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'layers/presentation/pages/Home.dart';
+import 'package:lembretes/layers/presentation/pages/MainPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Lembretes",
-      home: Home(),
+      home: MainPage(),
     );
   }
 }
