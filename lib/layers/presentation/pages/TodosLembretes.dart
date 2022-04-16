67603b1f13ea13fe3bdafd63532da6aefed59776
Vitoria <vick_santos_vick@hotmail.com>
2022-04-15 22:44:28 -0300

    criando a pagina de listar todos lembretes

---
+++ lib/layers/presentation/pages/TodosLembretes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodosLembretes extends StatefulWidget {
+  const TodosLembretes({Key key}) : super(key: key);
+
+  @override
+  State<TodosLembretes> createState() => _TodosLembretesState();
+}
+
+class _TodosLembretesState extends State<TodosLembretes> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
