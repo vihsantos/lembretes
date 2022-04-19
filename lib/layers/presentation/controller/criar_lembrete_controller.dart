273f682c95b1e63542068d8d39960acf1693181e
Vitoria <vick_santos_vick@hotmail.com>
2022-04-19 17:52:20 -0300

    criando a controller de criar lembrete e inserindo na page

--- lib/layers/presentation/controller/criar_lembrete_controller.dart
+++ lib/layers/presentation/controller/criar_lembrete_controller.dart
@@ -1,1 +1,12 @@
-class CriarLembreteController {}
+import '../../data/dto/lembrete_dto.dart';
+import '../../domain/usecases/PostLembrete/post_lembrete_usecase.dart';
+
+class CriarLembreteController {
+  final PostLembreteUseCase postLembreteUseCase;
+
+  CriarLembreteController(this.postLembreteUseCase);
+
+  postLembrete(LembreteDto lembrete) {
+    postLembreteUseCase.call(lembrete);
+  }
+}
--- lib/layers/presentation/pages/CriarLembrete.dart
+++ lib/layers/presentation/pages/CriarLembrete.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:lembretes/layers/data/datasources/remote/lembretes_datasource_imp.dart';
+import 'package:lembretes/layers/data/dto/lembrete_dto.dart';
+import 'package:lembretes/layers/data/repositories/LembreteRepositoryImp.dart';
+import 'package:lembretes/layers/domain/usecases/PostLembrete/post_lembrete_usecase.dart';
+import 'package:lembretes/layers/domain/usecases/PostLembrete/post_lembretes_usecase_imp.dart';
+import 'package:lembretes/layers/presentation/controller/criar_lembrete_controller.dart';
 import 'package:lembretes/layers/presentation/pages/MainPage.dart';
 import 'package:lembretes/layers/presentation/utils/PaletaDeCores.dart';
 
@@ -12,8 +19,10 @@ class CriarLembrete extends StatefulWidget {
 }
 
 class _CriarLembreteState extends State<CriarLembrete> {
-  TextEditingController tituloController;
-  TextEditingController descricaoController;
+  CriarLembreteController _controller = new CriarLembreteController(
+      PostLembreteUseCaseImp(LembreteRepositoryImp(LembretesDataSourceImp())));
+  TextEditingController tituloController = TextEditingController();
+  TextEditingController descricaoController = TextEditingController();
 
   final _formKey = GlobalKey<FormState>();
 
@@ -38,77 +47,80 @@ class _CriarLembreteState extends State<CriarLembrete> {
         padding: const EdgeInsets.only(left: 20, right: 20),
         child: Form(
           key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: 5),
-                width: size.width * 0.98,
-                height: size.height * 0.07,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: PaletaDeCores.roxo.withOpacity(0.3)),
-                child: TextFormField(
-                  controller: tituloController,
-                  onChanged: (value) {},
-                  decoration: InputDecoration(
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      hintText: "Insira um titulo"),
-                ),
-              ),
-              SizedBox(
-                height: size.height * 0.01,
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 5, bottom: 5, right: 5),
-                width: size.width * 0.98,
-                // height: size.height * 0.07,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: PaletaDeCores.roxo.withOpacity(0.3)),
-                child: TextFormField(
-                  controller: descricaoController,
-                  maxLength: 255,
-                  maxLines: 10,
-                  decoration: InputDecoration(
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      hintText: "Insira a descrição do seu lembrete"),
-                ),
-              ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: PaletaDeCores.roxo,
-                    shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(10.0),
-                    ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 5),
+                  width: size.width * 0.98,
+                  height: size.height * 0.07,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: PaletaDeCores.roxo.withOpacity(0.3)),
+                  child: TextFormField(
+                    controller: tituloController,
+                    onChanged: (value) {},
+                    decoration: InputDecoration(
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        hintText: "Insira um titulo"),
                   ),
-                  onPressed: () {
-                    // DateTime data = DateTime.now();
+                ),
+                SizedBox(
+                  height: size.height * 0.01,
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 5, bottom: 5, right: 5),
+                  width: size.width * 0.98,
+                  // height: size.height * 0.07,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: PaletaDeCores.roxo.withOpacity(0.3)),
+                  child: TextFormField(
+                    controller: descricaoController,
+                    maxLength: 255,
+                    maxLines: 10,
+                    decoration: InputDecoration(
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        hintText: "Insira a descrição do seu lembrete"),
+                  ),
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: PaletaDeCores.roxo,
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    onPressed: () async {
+                      DateTime data = DateTime.now();
 
-                    // Lembrete novo = new Lembrete(
-                    //   titulo: tituloController.text,
-                    //   descricao: descricaoController.text,
-                    //   datal: data,
-                    //   favorito: false,
-                    // );
+                      LembreteDto novo = new LembreteDto(
+                        titulo: tituloController.text,
+                        descricao: descricaoController.text,
+                        datal: data,
+                        favorito: false,
+                      );
 
-                    // tituloController.clear();
-                    // descricaoController.clear();
-                    // data = null;
+                      await _controller.postLembreteUseCase(novo);
 
-                    Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const MainPage()),
-                        (route) => false);
-                  },
-                  child: Text("Salvar",
-                      style:
-                          TextStyle(color: PaletaDeCores.branco, fontSize: 16)))
-            ],
+                      tituloController.clear();
+                      descricaoController.clear();
+                      data = null;
+
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const MainPage()),
+                          (route) => false);
+                    },
+                    child: Text("Salvar",
+                        style: TextStyle(
+                            color: PaletaDeCores.branco, fontSize: 16)))
+              ],
+            ),
           ),
         ),
       ),
