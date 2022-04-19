46deebc334c13279a998d2ee516b0877c939b338
Vitoria <vick_santos_vick@hotmail.com>
2022-04-19 17:49:45 -0300

    troca de nome do datasource

--- lib/layers/data/datasources/lembretes_datasource.dart
+++ lib/layers/data/datasources/lembretes_datasource.dart
@@ -1,6 +1,8 @@
 import 'package:lembretes/layers/data/dto/lembrete_dto.dart';
 
-abstract class GetLembretesDataSource {
+abstract class LembretesDataSource {
   Future<List<LembreteDto>> getLembretes();
   Future<void> postLembrete(LembreteDto lembrete);
+  Future<void> deletarLembrete(int id);
+  Future<List<LembreteDto>> buscarFavoritos();
 }
--- lib/layers/data/datasources/remote/lembretes_datasource_imp.dart
+++ lib/layers/data/datasources/remote/lembretes_datasource_imp.dart
@@ -1,9 +1,9 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:lembretes/layers/data/dto/lembrete_dto.dart';
-import '../get_lembretes_datasource.dart';
+import '../lembretes_datasource.dart';
 
-class GetLembretesDataSourceImp extends GetLembretesDataSource {
+class LembretesDataSourceImp extends LembretesDataSource {
   @override
   Future<List<LembreteDto>> getLembretes() async {
     var response =
@@ -26,8 +26,30 @@ class GetLembretesDataSourceImp extends GetLembretesDataSource {
 
     var response = await http.post(uri,
         headers: {"Content-Type": "application/json"},
-        body: json.encode(lembrete));
+        body: json.encode(lembrete.toMap()));
 
     if (response.statusCode == 200) print("Ok!!");
+  }
+
+  @override
+  Future<List<LembreteDto>> buscarFavoritos() async {
+    var response = await http
+        .get(Uri.parse('http://192.168.2.104:3000/lembretes/favoritos/'));
+
+    if (response.statusCode == 200) {
+      Iterable lista = json.decode(response.body);
+      List<LembreteDto> favoritos =
+          lista.map((model) => LembreteDto.fromMap(model)).toList();
+
+      return favoritos;
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  Future<void> deletarLembrete(int id) {
+    // TODO: implement deletarLembrete
+    throw UnimplementedError();
   }
 }
