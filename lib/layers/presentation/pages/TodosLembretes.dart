5f0a0c653178968e8d850e7932f22310c82b0358
Vitoria <vick_santos_vick@hotmail.com>
2022-04-19 18:10:32 -0300

    criando a controller da pagina de listar e a implementando

--- lib/layers/presentation/pages/TodosLembretes.dart
+++ lib/layers/presentation/pages/TodosLembretes.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:lembretes/layers/data/datasources/remote/lembretes_datasource_imp.dart';
+import 'package:lembretes/layers/data/repositories/LembreteRepositoryImp.dart';
+import 'package:lembretes/layers/domain/usecases/GetLembretes/get_lembretes_usecase_imp.dart';
+import 'package:lembretes/layers/presentation/controller/todos_lembretes_controller.dart';
 import 'package:lembretes/layers/presentation/utils/PaletaDeCores.dart';
+
+import '../../data/dto/lembrete_dto.dart';
+import '../utils/cards/SemLembretes.dart';
+import '../utils/cards/card_lembrete.dart';
 
 class TodosLembretes extends StatefulWidget {
   const TodosLembretes({Key key}) : super(key: key);
@@ -9,6 +17,8 @@ class TodosLembretes extends StatefulWidget {
 }
 
 class _TodosLembretesState extends State<TodosLembretes> {
+  TodosLembretesController _controller = new TodosLembretesController(
+      GetLembreteUseCaseImp(LembreteRepositoryImp(LembretesDataSourceImp())));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +33,31 @@ class _TodosLembretesState extends State<TodosLembretes> {
               fontSize: 22,
               color: PaletaDeCores.roxo,
               fontWeight: FontWeight.w900),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: ValueListenableBuilder(
+          valueListenable: _controller.loadingApi,
+          builder: (_, loading, __) {
+            if (loading) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (_controller.lembretes.length == null) {
+              return SemLembretes();
+            }
+
+            return ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: _controller.lembretes.length,
+                itemBuilder: (context, index) {
+                  LembreteDto lembrete = _controller.lembretes[index];
+
+                  return CardLembrete(lembrete: lembrete);
+                });
+          },
         ),
       ),
     );
