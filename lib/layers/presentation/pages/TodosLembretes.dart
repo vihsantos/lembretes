5e5a2e47273e2f27e1a150452eaecef4614c4124
Vitoria <vick_santos_vick@hotmail.com>
2022-04-19 17:52:40 -0300

    editando page de listar lembretes

--- lib/layers/presentation/pages/TodosLembretes.dart
+++ lib/layers/presentation/pages/TodosLembretes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lembretes/layers/presentation/utils/PaletaDeCores.dart';
 
 class TodosLembretes extends StatefulWidget {
   const TodosLembretes({Key key}) : super(key: key);
@@ -10,6 +11,20 @@ class TodosLembretes extends StatefulWidget {
 class _TodosLembretesState extends State<TodosLembretes> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: PaletaDeCores.background,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          "Todos Lembretes".toUpperCase(),
+          style: TextStyle(
+              fontSize: 22,
+              color: PaletaDeCores.roxo,
+              fontWeight: FontWeight.w900),
+        ),
+      ),
+    );
   }
 }
