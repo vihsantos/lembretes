46fb8ca717cce7b406f51b3b6ed9130c83c94bd9
Vitoria <vick_santos_vick@hotmail.com>
2022-04-19 17:51:54 -0300

    adicionando espaçamento em baixo do card lembrete

--- lib/layers/presentation/utils/cards/card_lembrete.dart
+++ lib/layers/presentation/utils/cards/card_lembrete.dart
@@ -23,88 +23,91 @@ class CardLembrete extends StatelessWidget {
             MaterialPageRoute(
                 builder: (context) => Detalhar(lembrete: lembrete)));
       }),
-      child: Container(
-        decoration: BoxDecoration(
-          color: PaletaDeCores.branco,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        width: size.width * 0.98,
-        height: size.height * 0.22,
-        child: Stack(children: [
-          Align(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      lembrete.titulo,
-                      style: TextStyle(
-                          color: PaletaDeCores.preto,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16),
-                    ),
-                    SizedBox(height: size.height * 0.01),
-                    Container(
-                      width: size.width * 0.79,
-                      height: size.height * 0.096,
-                      child: Text(
-                        lembrete.descricao,
-                        maxLines: 3,
-                        textAlign: TextAlign.justify,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: Container(
+          decoration: BoxDecoration(
+            color: PaletaDeCores.branco,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          width: size.width * 0.98,
+          height: size.height * 0.22,
+          child: Stack(children: [
+            Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        lembrete.titulo,
                         style: TextStyle(
                             color: PaletaDeCores.preto,
-                            fontSize: 16,
-                            fontStyle: FontStyle.italic),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
                       ),
-                    ),
-                  ],
-                ),
-              )),
-          Align(
-            alignment: Alignment.topRight,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                IconButton(
-                    onPressed: null,
-                    icon: Icon(
-                      Icons.favorite_outline,
-                      color: PaletaDeCores.roxo,
-                    )),
-                IconButton(
-                    onPressed: null,
-                    icon: Icon(
-                      Icons.edit,
-                      color: PaletaDeCores.roxo,
-                    )),
-              ],
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              decoration: BoxDecoration(
-                  color: PaletaDeCores.roxo,
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20))),
-              height: size.height * 0.048,
-              width: size.width * 0.98,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 10, top: 6, bottom: 6),
-                child: Text(
-                  DateFormat('dd/MM/yyyy').format(lembrete.datal),
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: PaletaDeCores.branco),
-                ),
+                      SizedBox(height: size.height * 0.01),
+                      Container(
+                        width: size.width * 0.79,
+                        height: size.height * 0.096,
+                        child: Text(
+                          lembrete.descricao,
+                          maxLines: 3,
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(
+                              color: PaletaDeCores.preto,
+                              fontSize: 16,
+                              fontStyle: FontStyle.italic),
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+            Align(
+              alignment: Alignment.topRight,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconButton(
+                      onPressed: null,
+                      icon: Icon(
+                        Icons.favorite_outline,
+                        color: PaletaDeCores.roxo,
+                      )),
+                  IconButton(
+                      onPressed: null,
+                      icon: Icon(
+                        Icons.edit,
+                        color: PaletaDeCores.roxo,
+                      )),
+                ],
               ),
             ),
-          )
-        ]),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: PaletaDeCores.roxo,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20))),
+                height: size.height * 0.048,
+                width: size.width * 0.98,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10, top: 6, bottom: 6),
+                  child: Text(
+                    DateFormat('dd/MM/yyyy').format(lembrete.datal),
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: PaletaDeCores.branco),
+                  ),
+                ),
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
